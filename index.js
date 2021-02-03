588c602bda662899458660776275731961a06824
shmuly11 <shmuly11@gmail.com>
2021-02-03 17:14:27 -0500

    commit tree 02829d63a663fcd9bca9c201f79ca66dc429359d

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+function moveDodgerLeft() {
+    // let leftNumbers = dodger.style.left.replace("px", "");
+    let leftNumbers = dodger.style.left
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 20}px`;
+    }
+    else if (left <= 0){
+        dodger.style.left = `${360}px`;
+     }
+    
+  }
+
+  function moveDodgerRight() {
+    // let leftNumbers = dodger.style.left.replace("px", "");
+    let leftNumbers = dodger.style.left
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 20}px`;
+    }
+     else if (left >= 360){
+        dodger.style.left = `${0}px`;
+     }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
